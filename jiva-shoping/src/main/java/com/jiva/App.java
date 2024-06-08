8f19a7477c6ba5b2ffb96869ec84a34eca0888b4
root <root@ip-172-31-29-199.ec2.internal>
2024-06-08 05:50:22 +0000

    shoping-app

---
+++ jiva-shoping/src/main/java/com/jiva/App.java
@@ -0,0 +1,13 @@
+package com.jiva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
